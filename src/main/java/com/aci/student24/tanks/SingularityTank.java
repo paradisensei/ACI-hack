5e317b78b8b66048b3f0f69e33165cf8fe8609fd
paradisensei <aidarshaifutdinov@gmail.com>
2016-11-26 12:05:40 +0300

    asd

--- src/main/java/com/aci/student24/tanks/SingularityTank.java
+++ src/main/java/com/aci/student24/tanks/SingularityTank.java
@@ -6,10 +6,7 @@ import com.aci.student24.api.tanks.objects.Tank;
 import com.aci.student24.api.tanks.state.MapState;
 import com.aci.student24.api.tanks.state.TankMove;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -20,6 +17,9 @@ public class SingularityTank implements Algorithm {
     private int teamId;
     private final int MAX_X = 64;
     private final int MAX_Y = 36;
+    private Map<Integer, Position> oldPos = new HashMap<>();
+    private boolean firstRun = true;
+    private boolean leftResp = false;
 
     @Override
     public void setMyId(int id) {
@@ -29,8 +29,62 @@ public class SingularityTank implements Algorithm {
     @Override
     public List<TankMove> nextMoves(MapState mapState) {
         List<Tank> tanks = mapState.getTanks(teamId);
-        Tank first, second;
-        Comparator<? super Tank> tankComparator = (Comparator<Tank>) (o1, o2) -> {
+        if (firstRun) {
+            leftResp = tanks.get(0).getX() < MAX_X / 2;
+        }
+        Tank first = getFirst(tanks);
+        Tank second = getSecond(tanks);
+        // remove special tanks
+        tanks.removeIf(t -> t.getId() == first.getId());
+        tanks.removeIf(t -> t.getId() == second.getId());
+        //TODO ebash!!!
+        List<TankMove> tankMoves = new ArrayList<>();
+        List<Tank> didntDoAnything;
+        if (firstRun) {
+            tanks.forEach(t -> oldPos.put(t.getId(), t.getPosition()));
+        } else {
+            didntDoAnything = tanks.stream()
+                    .filter(t -> t.getPosition().equals(oldPos.get(t.getId()).getPosition())
+                            && t.getDir() == t.getOldDir()).collect(Collectors.toList());
+        }
+        didntDoAnything
+        tanks.stream().map(t ->
+                new TankMove(t.getId(), leftResp ? (byte) 2 : (byte) 4, true)
+        ).collect(Collectors.toList());
+        if (leftResp) {
+            tankMoves.add(new TankMove(first.getId(), (byte) 1, true));
+            tankMoves.add(new TankMove(second.getId(), (byte) 3, true));
+        } else {
+            tankMoves.add(new TankMove(first.getId(), (byte) 3, true));
+            tankMoves.add(new TankMove(second.getId(), (byte) 1, true));
+        }
+        return tankMoves;
+    }
+
+    private Tank getFirst(List<Tank> tanks) {
+        Comparator<? super Tank> comparator = getComparator();
+        Optional<Tank> tank;
+        if (leftResp) {
+            tank = tanks.stream().min(comparator);
+        } else {
+            tank = tanks.stream().max(comparator);
+        }
+        return tank.get();
+    }
+
+    private Tank getSecond(List<Tank> tanks) {
+        Comparator<? super Tank> comparator = getComparator();
+        Optional<Tank> tank;
+        if (leftResp) {
+            tank = tanks.stream().max(comparator);
+        } else {
+            tank = tanks.stream().min(comparator);
+        }
+        return tank.get();
+    }
+
+    private Comparator<? super Tank> getComparator() {
+        return (Comparator<Tank>) (o1, o2) -> {
             if (o1.getY() < o2.getY()) {
                 return -1;
             }
@@ -39,24 +93,6 @@ public class SingularityTank implements Algorithm {
             }
             return 0;
         };
-        boolean leftResp = tanks.get(0).getX() < MAX_X / 2;
-        if (leftResp) {
-            Optional<Tank> bottomTank = tanks.stream().max(tankComparator);
-            Optional<Tank> upperTank = tanks.stream().min(tankComparator);
-            first = upperTank.get();
-            second = bottomTank.get();
-        } else {
-            Optional<Tank> bottomTank = tanks.stream().max(tankComparator);
-            Optional<Tank> upperTank = tanks.stream().min(tankComparator);
-            first = bottomTank.get();
-            second = upperTank.get();
-        }
-        tanks.remove(first);
-        tanks.remove(second);
-        List<TankMove> tankMoves = new ArrayList<>();
-        tanks.stream().map(t ->
-            new TankMove(t.getId(), leftResp ? (byte) 2 : (byte) 4, true)
-        ).collect(Collectors.toList());
-
     }
+
 }
